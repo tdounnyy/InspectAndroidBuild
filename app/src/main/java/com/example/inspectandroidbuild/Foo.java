d2d115ac11c770282303f0c9b490014c541f320e
duan.yufei <duanyufei@bytedance.com>
2020-12-09 11:58:15 +0800

    Add proguard

---
+++ app/src/main/java/com/example/inspectandroidbuild/Foo.java
@@ -0,0 +1,7 @@
+package com.example.inspectandroidbuild;
+
+class Foo {
+    public static String sayHello() {
+        return "Hello from Java";
+    }
+}
--- app/src/main/java/com/example/inspectandroidbuild/MainActivity.kt
+++ app/src/main/java/com/example/inspectandroidbuild/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.inspectandroidbuild
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("felixx", Foo.sayHello())
     }
 }
